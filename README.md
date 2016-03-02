84e4e8dfb0f05ffb942239a79388d6126ccdab4d
Dominiek Ter Heide <dominiek@bottlenose.com>
2016-03-02 14:51:44 -0800

    Cleaned up some code in ScatterPlot2d

--- README.md
+++ README.md
@@ -4,7 +4,10 @@
 
 - 2D zooming and panning
 - 2D display options: show labels
-- 2D display options: show labels
+- Drill-down of vector that shows real distance between neighbors
 - Visualize model without distance seeds (use sample rating)
 - Make work with non-Gensim models
 - Add documentation
+- 3D WebGL view
+- Comparisons between different vectors
+- More advanced querying
--- ui/components/ScatterPlot2d.jsx
+++ ui/components/ScatterPlot2d.jsx
@@ -9,15 +9,10 @@ export default React.createClass({
     }
   },
   componentDidMount() {
-    console.log("this.state.data.reduction", this.state.data.reduction[0])
     this._renderD3()
   },
   componentWillUpdate() {
-    console.log("this.state.data.reduction", this.state.data.reduction[0])
     this._updateD3()
-  },
-  componentWillUmount() {
-
   },
   render() {
     return (
@@ -26,82 +21,58 @@ export default React.createClass({
     )
   },
   _renderD3() {
-    console.log('_renderD3')
     var dataset = this.state.data.reduction
 
-    var $scatterPlot2dElement = $(this.refs.scatterPlot2dElement)
-    // Setup settings for graphic
-    var canvas_width = $scatterPlot2dElement.width()
-    var canvas_height = $scatterPlot2dElement.height()
-    var padding = 30
     var colors = this.props.color
+    var $scatterPlot2dElement = $(this.refs.scatterPlot2dElement)
+    var width = $scatterPlot2dElement.width()
+    var height = $scatterPlot2dElement.height()
+    var padding = 30
 
-    console.log(`ScatterPlot2d canvas_width=${canvas_width}, canvas_height=${canvas_height}, dataset.length=${dataset.length}`)
+    console.log(`ScatterPlot2d width=${width}, height=${height}, dataset.length=${dataset.length}`)
 
-    // Create scale functions
-    var xScale = d3.scale.linear()  // xScale is width of graphic
-       .domain([d3.min(dataset, function(d) {
-           return d[0];  // input domain
-       }), d3.max(dataset, function(d) {
-           return d[0];  // input domain
-       })])
-       .range([padding, canvas_width - padding]); // output range
+    var xScale = d3.scale.linear()
+      .domain([d3.min(dataset, d => d[0]), d3.max(dataset, d => d[0])])
+      .range([padding, width - padding]);
 
-    var yScale = d3.scale.linear()  // yScale is height of graphic
-       .domain([d3.min(dataset, function(d) {
-           return d[1];  // input domain
-       }), d3.max(dataset, function(d) {
-           return d[1];  // input domain
-       })])
-       .range([canvas_height - padding, padding]);  // remember y starts on top going down so we flip
+    var yScale = d3.scale.linear()
+      .domain([d3.min(dataset, d => d[1]), d3.max(dataset, d => d[1])])
+      .range([height - padding, padding]);
 
-    // Define X axis
     var xAxis = d3.svg.axis()
-                   .scale(xScale)
-                   .orient("bottom")
-                   .ticks(5);
+      .scale(xScale)
+      .orient("bottom")
+      .ticks(5);
 
-    // Define Y axis
     var yAxis = d3.svg.axis()
-                   .scale(yScale)
-                   .orient("left")
-                   .ticks(5);
+      .scale(yScale)
+      .orient("left")
+      .ticks(5);
 
-    // Create SVG element
-    var svg = d3.select("#scatter-plot-2d")  // This is where we put our vis
-       .append("svg")
-       .attr("width", canvas_width)
-       .attr("height", canvas_height)
+    var svg = d3.select("#scatter-plot-2d")
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height)
 
-    // Create Circles
     svg.selectAll("circle")
-     .data(dataset)
-     .enter()
-     .append("circle")  // Add circle svg
-     .attr("cx", function(d) {
-         return xScale(d[0]);  // Circle's X
-     })
-     .attr("cy", function(d) {  // Circle's Y
-         return yScale(d[1]);
-     })
-     .attr("fill", (d, i) => {
-       return colors(this.state.data.clusters[i])
-     })
-     .attr("r", 2);  // radius
+      .data(dataset)
+      .enter()
+      .append("circle")
+      .attr("cx", d => xScale(d[0]))
+      .attr("cy", d => yScale(d[1]))
+      .attr("fill", (d, i) => colors(this.state.data.clusters[i]))
+      .attr("r", 2);
 
-    // Add to X axis
     svg.append("g")
-       .attr("class", "x axis")
-       .attr("transform", "translate(0," + (canvas_height - padding) +")")
-       .call(xAxis);
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + (height - padding) +")")
+      .call(xAxis);
 
-    // Add to Y axis
     svg.append("g")
-       .attr("class", "y axis")
-       .attr("transform", "translate(" + padding +",0)")
-       .call(yAxis);
+      .attr("class", "y axis")
+      .attr("transform", "translate(" + padding +",0)")
+      .call(yAxis);
 
-    // On click, update with new data
     function update() {
        var self = this;
        var dataset = this.state.data.reduction
@@ -109,65 +80,41 @@ export default React.createClass({
 
        console.log(`ScatterPlot2d update, dataset.length=${dataset.length}`)
 
-       // Update scale domains
-       xScale.domain([d3.min(dataset, function(d) {
-           return d[0]; }), d3.max(dataset, function(d) {
-           return d[0]; })]);
-       yScale.domain([d3.min(dataset, function(d) {
-           return d[1]; }), d3.max(dataset, function(d) {
-           return d[1]; })]);
+       xScale.domain([d3.min(dataset, d => d[0]), d3.max(dataset, d => d[0])])
+       yScale.domain([d3.min(dataset, d => d[1]), d3.max(dataset, d => d[1])])
 
        // Update data for existing nodes
        var nodes = svg.selectAll("circle")
-         .data(dataset)  // Update with new data
+         .data(dataset)
 
        // Change existing nodes
        nodes
-         .transition()  // Transition from old to new
-         .duration(1000)  // Length of animation
-         .delay(function(d, i) {
-             return i / dataset.length * 500;  // Dynamic delay (i.e. each item delays a little longer)
-         })
-         //.ease("linear")  // Transition easing - default 'variable' (i.e. has acceleration), also: 'circle', 'elastic', 'bounce', 'linear'
-         .attr("cx", function(d) {
-             return xScale(d[0]);  // Circle's X
-         })
-         .attr("cy", function(d) {
-             return yScale(d[1]);  // Circle's Y
-         })
-         .attr("fill", (d, i) => {
-           return colors(this.state.data.clusters[i])
-         })
+         .transition()
+         .duration(1000)
+         .delay((d, i) => i / dataset.length * 500)
+         .attr("cx", d => xScale(d[0]))
+         .attr("cy", d => yScale(d[1]))
+         .attr("fill", (d, i) => colors(this.state.data.clusters[i]))
 
        // Render new nodes
        nodes
          .enter()
-         .append("circle")  // Add circle svg
-         .transition()  // Transition from old to new
-         .duration(1000)  // Length of animation
-         .delay(function(d, i) {
-             return i / dataset.length * 500;  // Dynamic delay (i.e. each item delays a little longer)
+         .append("circle")
+         .transition()
+         .duration(1000)
+         .delay((d, i) => i / dataset.length * 500)
+         .each("start", function() {
+            d3.select(this)
+              .attr("fill", "white")
          })
-         .each("start", function() {  // Start animation
-            d3.select(this)  // 'this' means the current element
-              .attr("fill", "white")  // Change color
-         })
-         .each("end", function(d, i) {  // Start animation
+         .each("end", function(d, i) {
            d3.select(this)
-             .attr("fill", () => {
-               return colors(self.state.data.clusters[i])
-             })
+             .attr("fill", () => colors(self.state.data.clusters[i]))
          })
-         .attr("cx", function(d) {
-           return xScale(d[0]);  // Circle's X
-         })
-         .attr("cy", function(d) {  // Circle's Y
-           return yScale(d[1]);
-         })
-         .attr("fill", (d, i) => {
-           return colors(self.state.data.clusters[i])
-         })
-         .attr("r", 2);  // Change size
+         .attr("cx", d => xScale(d[0]))
+         .attr("cy", d => yScale(d[1]))
+         .attr("fill", (d, i) => colors(this.state.data.clusters[i]))
+         .attr("r", 2);
 
       // Remove old nodes
       nodes
@@ -188,6 +135,5 @@ export default React.createClass({
     };
 
     this._updateD3 = update
-    window.updateD3 = update
   }
 });
