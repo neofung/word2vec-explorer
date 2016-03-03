5b71a504c57f438ce79558106a7fbc351c5558c2
Dominiek Ter Heide <dominiek@bottlenose.com>
2016-03-02 16:30:06 -0800

    Zooming and panning

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 
-
 # Todo
 
-- 2D zooming and panning
 - 2D display options: show labels
 - Drill-down of vector that shows real distance between neighbors
 - Visualize model without distance seeds (use sample rating)
 - Make work with non-Gensim models
+- Enable JS Lint
+- Add requirements.txt for pip
 - Add documentation
 - 3D WebGL view
 - Comparisons between different vectors
--- ui/components/ScatterPlot2d.jsx
+++ ui/components/ScatterPlot2d.jsx
@@ -20,6 +20,13 @@ export default React.createClass({
       </div>
     )
   },
+  _nodeRadius: function() {
+    var nodeRadius = 2
+    if (this.state.data.reduction.length > 5000) nodeRadius = 1
+    if (this.state.data.reduction.length < 500) nodeRadius = 3
+    if (this.state.data.reduction.length < 50) nodeRadius = 4
+    return nodeRadius
+  },
   _renderD3() {
     var dataset = this.state.data.reduction
 
@@ -30,6 +37,33 @@ export default React.createClass({
     var padding = 30
 
     console.log(`ScatterPlot2d width=${width}, height=${height}, dataset.length=${dataset.length}`)
+
+    var zoom = d3.behavior.zoom()
+      .scaleExtent([1, 10])
+      .on("zoom", zoomed);
+
+    var drag = d3.behavior.drag()
+      .origin(function(d) { return d; })
+      .on("dragstart", dragstarted)
+      .on("drag", dragged)
+      .on("dragend", dragended);
+
+    function zoomed() {
+      container.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+    }
+
+    function dragstarted(d) {
+      d3.event.sourceEvent.stopPropagation();
+      d3.select(this).classed("dragging", true);
+    }
+
+    function dragged(d) {
+      d3.select(this).attr("cx", d.x = d3.event.x).attr("cy", d.y = d3.event.y);
+    }
+
+    function dragended(d) {
+      d3.select(this).classed("dragging", false);
+    }
 
     var xScale = d3.scale.linear()
       .domain([d3.min(dataset, d => d[0]), d3.max(dataset, d => d[0])])
@@ -53,25 +87,38 @@ export default React.createClass({
       .append("svg")
       .attr("width", width)
       .attr("height", height)
+      .append("g")
+      .attr("transform", "translate(" + padding + "," + padding + ")")
+      .call(zoom);
 
-    svg.selectAll("circle")
+    var backdrop = svg.append("rect")
+      .attr("width", width)
+      .attr("height", height)
+      .style("fill", "white")
+      .style("pointer-events", "all");
+
+    var container = svg.append("g");
+
+    container.selectAll("circle")
       .data(dataset)
       .enter()
       .append("circle")
       .attr("cx", d => xScale(d[0]))
       .attr("cy", d => yScale(d[1]))
       .attr("fill", (d, i) => colors(this.state.data.clusters[i]))
-      .attr("r", 2);
+      .attr("r", this._nodeRadius());
 
-    svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + (height - padding) +")")
-      .call(xAxis);
+    if (this.props.drawAxes) {
+      container.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + (height - padding) +")")
+        .call(xAxis);
 
-    svg.append("g")
-      .attr("class", "y axis")
-      .attr("transform", "translate(" + padding +",0)")
-      .call(yAxis);
+      container.append("g")
+        .attr("class", "y axis")
+        .attr("transform", "translate(" + padding +",0)")
+        .call(yAxis);
+    }
 
     function update() {
        var self = this;
@@ -84,7 +131,7 @@ export default React.createClass({
        yScale.domain([d3.min(dataset, d => d[1]), d3.max(dataset, d => d[1])])
 
        // Update data for existing nodes
-       var nodes = svg.selectAll("circle")
+       var nodes = container.selectAll("circle")
          .data(dataset)
 
        // Change existing nodes
@@ -95,6 +142,7 @@ export default React.createClass({
          .attr("cx", d => xScale(d[0]))
          .attr("cy", d => yScale(d[1]))
          .attr("fill", (d, i) => colors(this.state.data.clusters[i]))
+         .attr("r", this._nodeRadius());
 
        // Render new nodes
        nodes
@@ -114,7 +162,7 @@ export default React.createClass({
          .attr("cx", d => xScale(d[0]))
          .attr("cy", d => yScale(d[1]))
          .attr("fill", (d, i) => colors(this.state.data.clusters[i]))
-         .attr("r", 2);
+         .attr("r", this._nodeRadius());
 
       // Remove old nodes
       nodes
@@ -122,13 +170,13 @@ export default React.createClass({
         .remove();
 
        // Update X Axis
-       svg.select(".x.axis")
+       container.select(".x.axis")
          .transition()
          .duration(1000)
          .call(xAxis);
 
        // Update Y Axis
-       svg.select(".y.axis")
+       container.select(".y.axis")
          .transition()
          .duration(100)
          .call(yAxis);
